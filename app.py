d8269ab6d643fe778e86b3c7e0acf0e0f2c361f0
phamkdaniel <phamkdaniel@gmail.com>
2019-12-13 15:11:36 -0800

    update initial files to new version

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
 
-# reflect an existing database into a new model
+# # reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+# # reflect the tables
 Base.prepare(db.engine, reflect=True)
 
-# Save references to each table
+# # Save references to each table
 Samples_Metadata = Base.classes.sample_metadata
 Samples = Base.classes.samples
 
@@ -102,4 +102,5 @@ def samples(sample):
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
